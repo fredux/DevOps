7ce00a17eff70063e5f335a1bb130d1bc727ee1b
Ubuntu <ubuntu@ip-172-31-29-124.ec2.internal>
2021-08-17 14:38:43 +0000

    firs commit

---
+++ terraform-dynamoDB/dynamoDB.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "dynamoDB" {
+  name   = "cliente"
+#  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key   = "cpf"
+#  range_key  = "name"
+  
+  attribute {
+    name = "cpf"
+    type = "S"
+  }  
+  
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+
+ #local_secondary_indexes = [{
+ #   name               = "customerId-city-index"
+ #   range_key          = "city"
+ #   projection_type    = "ALL"                   # ALL, INCLUDE or KEYS_ONLY
+ #   non_key_attributes = []
+ # }]
+  
+#  global_secondary_index {
+#    name               = "GameTitleIndex"
+#    hash_key           = "GameTitle"
+#    range_key          = "TopScore"
+#    write_capacity     = 10
+#    read_capacity      = 10
+#    projection_type    = "INCLUDE"
+#    non_key_attributes = ["UserId"]
+#  }
+
+  tags = {
+    Name        = "dynamodb-book"
+    Environment = "dev"
+  }
+}
